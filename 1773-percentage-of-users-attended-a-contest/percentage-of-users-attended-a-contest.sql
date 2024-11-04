22949ace436321be7cd0539c3a8dd39115c66ee0
Jithesh598 <130023278+Jithesh598@users.noreply.github.com>
2024-11-04 10:16:59 -0800

    Time: 1015 ms (71.24%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -3,3 +3,5 @@
 select register.contest_id , round(count(distinct(register.user_id)) * 100 / (select count(*) from users) ,2) as percentage from register 
 group by register.contest_id
 order by percentage desc, register.contest_id asc;
+
+
