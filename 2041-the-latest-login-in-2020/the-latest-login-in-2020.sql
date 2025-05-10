613ecd021499040038bfe21c1df287d693fe78ca
Jithesh598 <130023278+Jithesh598@users.noreply.github.com>
2025-05-09 21:41:22 -0700

    Time: 696 ms (45.79%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+with ss as (
+select user_id, time_stamp, rank() over(partition by user_id order by time_stamp desc) as ran from logins
+where year(time_stamp) = 2020 
+)
+select user_id, time_stamp as last_stamp from ss
+where ran = 1 ;
