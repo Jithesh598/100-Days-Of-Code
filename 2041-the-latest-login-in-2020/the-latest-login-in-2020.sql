1085b9ef8fcbdba5a85e7e112826c99c4d91f907
Jithesh598 <130023278+Jithesh598@users.noreply.github.com>
2025-05-09 21:44:53 -0700

    Time: 661 ms (59.26%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,7 +1,12 @@
 # Write your MySQL query statement below
-with ss as (
-select user_id, time_stamp, row_number() over(partition by user_id order by time_stamp desc) as ran from logins
-where year(time_stamp) = 2020 
-)
-select user_id, time_stamp as last_stamp from ss
-where ran = 1 ;
+-- with ss as (
+-- select user_id, time_stamp, row_number() over(partition by user_id order by time_stamp desc) as ran from logins
+-- where year(time_stamp) = 2020 
+-- )
+-- select user_id, time_stamp as last_stamp from ss
+-- where ran = 1 ;
+
+SELECT user_id, MAX(time_stamp) AS last_stamp
+FROM logins
+WHERE YEAR(time_stamp) = 2020
+GROUP BY user_id;
