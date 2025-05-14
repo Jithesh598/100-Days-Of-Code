9759f451ebbbb74bae6dcac202dad644d3e95d21
Jithesh598 <130023278+Jithesh598@users.noreply.github.com>
2025-05-13 18:29:16 -0700

    Time: 1221 ms (83.92%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -26,25 +26,42 @@
 -- group by buyer_id, join_date
 -- order by buyer_id asc;
 
-WITH ss AS (
-  SELECT 
-    u.user_id, 
-    u.join_date, 
-    o.order_date
-  FROM users u
-  LEFT JOIN orders o ON u.user_id = o.buyer_id 
-)
-SELECT 
-  user_id AS buyer_id,
-  join_date,
-  COUNT(CASE 
-           WHEN YEAR(order_date) = 2019 THEN 1 
-       END) AS orders_in_2019
-FROM ss 
-GROUP BY user_id, join_date
-ORDER BY user_id;
+-- WITH ss AS (
+--   SELECT 
+--     u.user_id, 
+--     u.join_date, 
+--     o.order_date
+--   FROM users u
+--   LEFT JOIN orders o ON u.user_id = o.buyer_id 
+-- )
+-- SELECT 
+--   user_id AS buyer_id,
+--   join_date,
+--   COUNT(CASE 
+--            WHEN YEAR(order_date) = 2019 THEN 1 
+--        END) AS orders_in_2019
+-- FROM ss 
+-- GROUP BY user_id, join_date
+-- ORDER BY user_id;
 
+---
 
 -- select buyer_id, join_date, ifnull(sum(cnt),0) as orders_in_2019 from sa
 -- group by buyer_id, join_date
 -- order by buyer_id asc;
+
+with ss as (
+select u.user_id, u.join_date, o.order_date from users u
+left join orders o on u.user_id = o.buyer_id 
+),
+sa as (
+select user_id as buyer_id, join_date,
+case
+     when year(order_date) = 2019 then 1
+     else 0
+end as cnt
+from ss 
+)
+select buyer_id, join_date, sum(cnt) as orders_in_2019 from sa
+group by buyer_id, join_date
+order by buyer_id asc;
